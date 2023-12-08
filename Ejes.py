4388f8bfe2b3402794965aafd30266074dc12f43
PRO X <bredalisgautreaux@gmail.com>
2023-12-08 06:42:15 -0400

    Archivos agregados

---
+++ Ejes.py
@@ -0,0 +1,22 @@
+
+# Libreria
+
+import numpy as np
+
+matriz_2D = np.array([[0, 1, 2], [4, 2, 3]])
+altura = np.array([1.74, 1.8, 1.78, 1.68, 1.78])
+peso = np.array([81.4, 88.7, 87.3, 62.7, 81.6])
+
+union = np.stack((altura, peso), axis = 0)
+separacion = np.split(union, 2)
+
+# Mostrar datos
+
+print("Union: \n", np.stack((altura, peso), axis = 1))
+print("\nUnion: \n", np.concatenate((altura, peso)))
+
+print("\nUnion: \n", union, type(union))
+print("\nSeparacion: \n", separacion, type(separacion))
+
+print("\nSuma de filas: \n", np.sum(matriz_2D, axis = 0))	
+print("\nSuma de columnas: \n", np.sum(matriz_2D, axis = 1))
---
+++ Matrices.py
@@ -0,0 +1,19 @@
+
+# Libreria
+
+import numpy as np
+
+# Listas y matrices
+
+lista_impar = [1, 3, 5]
+matriz_par = np.array([2, 4, 6])
+
+# Agregar elementos
+
+lista_impar.append(7)
+matriz_par = np.append(matriz_par, 8)
+
+# Mostrar datos
+
+print(f"Lista Impar: {lista_impar}")
+print(f"Matriz par: {matriz_par}")
